7803cbf3323123513e64d5643fd4e6ca5d5f2dac
jainilsoni1706 <75731365+jainilsoni1706@users.noreply.github.com>
2021-12-31 21:55:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,40 +6,35 @@ const dx = new Date();
 },1000);
 
 
-  				const dz = new Date();
+  				let dz = new Date();
   				let dz1 = dz.getDay();
   			let day = document.getElementById("day");
   			const day_array = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-switch(dz1){
-				case 0:{
-								day.innerHTML = day_array[0];
-				}
-				case 1:{
-								day.ineerHTML = day_array[1];
-				}
-				case 2:{
-								day.innerHTML = day_array[2];
-				}
-				case 3:{
-								day.innerHTML = day_array[3];
-				}
-				case 4:{
-								day.innerHTML = day_array[4];
-				}
-				case 5:{
-								day.innerHTML = day_array[5];
-				}
-				case 6:{
-								day.innerHTML = day_array[6];
-				}
-				default:{
-								
-				}
+
+if(dz1===0){
+				day.innerHTML = day_array[0];
 }
-  		
-/*var msg = new SpeechSynthesisUtterance(); msg.text = "Hello There! Welcom to the official site of Mister Jainil Soni. My name is JAVA, Jainils Artificial virtual Assistant and i am a virtual assistant."; window.speechSynthesis.speak(msg);
-*/
+else if(dz1===1) {
+				day.innerHTML = day_array[1];
+}
+else if(dz1===2) {
+				day.innerHTML = day_array[2];
+}
+else if(dz1===3) {
+				day.innerHTML = day_array[3];
+}
+else if(dz1===4) {
+				day.innerHTML = day_array[4];
+}
+else if(dz1===5) {
+				day.innerHTML = day_array[5];
+}
+else{
+				day.innerHTML = day_array[6];
+}
+
+
 
 let dw = new Date();
 document.getElementById("date").innerHTML = dw.toLocaleDateString();
